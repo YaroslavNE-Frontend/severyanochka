9eeff5c3ac9c92df49ccd0150b1cd1a426b32d21
Ярослав Новиков <yaroslav.nov2001@gmail.com>
2023-11-07 14:47:42 +0900

    modal js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@
 //     CatalogList.classList.toggle('active');
 // }
 
+
+// const NewBtn = document.querySelector('new-btn');
+// const Modal = document.querySelector('modal__wrapper');
+
+// NewBtn.onclick  = () => {
+//     Modal.classList.toggle('none');
+// }
+
+
+const OpenBtn = document.querySelector('.sign-in');
+const ModalWrapper  = document.querySelector('.modal__wrapper');
+const CloseBtn = document.querySelector('.icon-close-btn');
+
+OpenBtn.addEventListener('click', (e) => {
+    ModalWrapper.classList.toggle('none');
+});
+
+CloseBtn.addEventListener('click', (e) => {
+    ModalWrapper.classList.toggle('none');
+});
+
+
