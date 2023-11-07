b36c3727770e0c7d59faea8a5b86b4b9b3e49fd3
Ярослав Новиков <yaroslav.nov2001@gmail.com>
2023-11-07 18:01:09 +0900

    search js

--- js/main.js
+++ js/main.js
@@ -14,8 +14,11 @@
 // }
 
 
+// МОДАЛЬНОЕ ОКНО
+
+
 const OpenBtn = document.querySelector('.sign-in');
-const ModalWrapper  = document.querySelector('.modal__wrapper');
+const ModalWrapper = document.querySelector('.modal__wrapper');
 const CloseBtn = document.querySelector('.icon-close-btn');
 
 OpenBtn.addEventListener('click', (e) => {
@@ -29,17 +32,35 @@ CloseBtn.addEventListener('click', (e) => {
 
 // ПОИСК
 
-document.querySelector('#product').oninput = function() {
+document.querySelector('#product').oninput = function () {
     let val = this.value.trim();
     let productItem = document.querySelectorAll('.product-list li');
     if (val != '') {
-        productItem.forEach(function(elem) {
+        productItem.forEach(function (elem) {
             if (elem.innerText.search(val) == -1) {
-                elem.classList.add('none')
+                elem.classList.add('none');
+                // elem.innerHTML = elem.innerText;
             }
-            else elem.classList.remove('none');
+            else {
+                elem.classList.remove('none');
+                // let str = elem.innerText;
+                // elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length);
+            }
+        });
+    }
+    else {
+        productItem.forEach(function (elem) {
+            elem.classList.remove('none');
+            // elem.innerHTML = elem.innerText;
         });
     }
 }
+
+// ВЫДЕЛЕНИЕ ТЕКСТА ПРИ ВВОДЕ В ПОИСК
+
+// function insertMark(string, pos, len) {
+//     return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
+// }
+
 
 
