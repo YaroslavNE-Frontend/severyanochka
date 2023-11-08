de696db92556f7ab5a1080775729bef42738132c
Ярослав Новиков <yaroslav.nov2001@gmail.com>
2023-11-08 13:07:41 +0900

    search js full

--- js/main.js
+++ js/main.js
@@ -21,6 +21,16 @@ const OpenBtn = document.querySelector('.sign-in');
 const ModalWrapper = document.querySelector('.modal__wrapper');
 const CloseBtn = document.querySelector('.icon-close-btn');
 
+
+
+// OpenBtn.forEach(function (item) {
+//     item.addEventListener('click', function () {
+//         const modalId = this.OpenBtn;
+//         const modal = document.querySelector('#' + modalId);
+//         modal.classList.remove('none')
+//     })
+// })
+
 OpenBtn.addEventListener('click', (e) => {
     ModalWrapper.classList.toggle('none');
 });
@@ -34,33 +44,33 @@ CloseBtn.addEventListener('click', (e) => {
 
 document.querySelector('#product').oninput = function () {
     let val = this.value.trim();
-    let productItem = document.querySelectorAll('.product-list li');
+    let productItem = document.querySelectorAll('.product-text');
     if (val != '') {
         productItem.forEach(function (elem) {
             if (elem.innerText.search(val) == -1) {
                 elem.classList.add('none');
-                // elem.innerHTML = elem.innerText;
+                elem.innerHTML = elem.innerText;
             }
             else {
                 elem.classList.remove('none');
-                // let str = elem.innerText;
-                // elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length);
+                let str = elem.innerText;
+                elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length);
             }
         });
     }
     else {
         productItem.forEach(function (elem) {
             elem.classList.remove('none');
-            // elem.innerHTML = elem.innerText;
+            elem.innerHTML = elem.innerText;
         });
     }
 }
 
 // ВЫДЕЛЕНИЕ ТЕКСТА ПРИ ВВОДЕ В ПОИСК
 
-// function insertMark(string, pos, len) {
-//     return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
-// }
+function insertMark(string, pos, len) {
+    return string.slice(0, pos) + '<span class="excretion-text">' + string.slice(pos, pos + len) + '</span>' + string.slice(pos + len);
+}
 
 
 
