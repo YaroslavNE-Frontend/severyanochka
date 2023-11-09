c583428cd2093b9cf46d3281fc91d27cdb532ad4
Ярослав Новиков <yaroslav.nov2001@gmail.com>
2023-11-09 20:12:51 +0900

    gap btn map

--- js/main.js
+++ js/main.js
@@ -117,29 +117,6 @@ OurshopsMapReset.addEventListener('click', event => {
 
 
 
-// OurshopsLink.classList.add("active");
-
-// let activePhoto = document.querySelector(".active-photo");
-// let previews = document.querySelectorAll(".preview-list a");
-
-// for (let preview of previews) {
-//     preview.onclick = function (evt) {
-//         evt.preventDefault();
-
-//         let currentActive = document.querySelector(".preview-list .active-item");
-//         currentActive.classList.remove("active-item");
-//         preview.classList.add("active-item");
-
-//         activePhoto.src = preview.href;
-//     };
-// }
-
-
-
-
-
-
-
 
 
 
